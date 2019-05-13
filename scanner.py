ade9f752bb4a5ea5cb7afe84576663b8457af9c5
Alex Gokan <amgokan@gmail.com>
2019-05-13 01:28:15 -0400

    finished scanning, compartmentalized into 2 files

---
+++ scanner.py
@@ -0,0 +1,230 @@
+
+class Scanner:
+    def __init__(self,byte_arr):
+        self.start = 0
+        self.current = 0
+        self.line = 1
+
+        self.byte_arr = byte_arr#called 'source' in book
+        self.tokens = []
+
+        self.keywords = [b'and',b'class',b'else',b'false',b'for',b'fun',b'if',b'nil',b'or',b'print',b'return',b'super',b'this',b'true',b'var',b'while']
+
+
+    def print_byte_arr(self):
+        for i in range(0,len(self.byte_arr)):
+            print(self.byte_arr[i])
+
+    def is_at_end(self):
+        return (self.current >= len(self.byte_arr))
+
+    def scan_tokens(self):
+        print("scanning tokens")
+        while(self.is_at_end() == False):
+            self.start = self.current
+            self.scan_token()
+
+
+        (self.tokens).append(Token("EOF","",None,None))#I think?
+        #in book it is (EOF,"","NULL","line")
+        return(self.tokens)
+        #must return some list of token objects
+
+    def scan_token(self):
+        c = self.advance()
+        if(c==40):
+            self.add_token('(',None)
+        elif(c==41):
+            self.add_token('(',None)
+        elif(c==123):
+            self.add_token('{',None)
+        elif(c==125):
+            self.add_token('}',None)
+        elif(c==44):
+            self.add_token(',',None)
+        elif(c==46):
+            self.add_token('.',None)
+        elif(c==45):
+            self.add_token('-',None)
+        elif(c==43):
+            self.add_token('+',None)
+        elif(c==59):
+            self.add_token(';',None)
+        elif(c==42):
+            self.add_token('*',None)
+        elif(c==33):
+            if(self.match(61)):
+                self.add_token('!=',None)
+            else:
+                self.add_token('!',None)
+        elif(c==61):
+            if(self.match(61)):
+                self.add_token('==',None)
+            else:
+                self.add_token('=',None)
+        elif(c==60):
+            if(self.match(61)):
+                self.add_token('<=',None)
+            else:
+                self.add_token('<',None)
+        elif(c==62):
+            if(self.match(61)):
+                self.add_token('>=',None)
+            else:
+                self.add_token('>',None)
+        elif(c==47):#comment
+            if(self.match(47)):
+                while(self.peek() != '\n' and (self.is_at_end() == False)):
+                    self.advance()
+            else:
+                self.add_token('/',None)
+        elif(c==32):#space
+            pass
+        elif(c==9):#tab
+            pass
+        elif(c==13):#carriage return
+            pass
+        elif(c==10):#newline
+            self.line += 1
+        elif(c==34):#double quotes
+            self.scan_string()
+        elif(self.is_digit(c)):
+            self.scan_number()
+        elif(self.is_alphabetic(c)):
+            self.scan_indentifier()
+        else:
+            Z.error_message(self.line,"unexpected character")
+
+    def scan_indentifier(self):
+        while(self.is_alphanumeric(self.peek())):
+            self.advance()
+
+        text = self.byte_arr[self.start:self.current:1]
+        text = bytes(text)
+        print(text)
+        if(text in self.keywords):
+            print("keyword detected")
+            type = text
+        else:
+            type = 'identifier'
+
+        self.add_token(type,None)
+
+    def is_alphabetic(self,c):
+        if((c >= 97 and c <= 122) or (c >= 65 and c<=90) or (c == 95)):
+            return True
+        else:
+            return False
+
+    def is_alphanumeric(self,c):
+        return (self.is_alphabetic(c) or self.is_digit(c))
+
+    def scan_number(self):
+        while(self.is_digit(self.peek())):
+            self.advance()
+
+        if(self.peek() == 46 and self.is_digit(self.peek_next())):
+            self.advance()
+
+            while(self.is_digit(self.peek())):
+                self.advance()
+
+        value = self.byte_arr[self.start:self.current:1]
+        value = self.array_to_double(value)
+        self.add_token('number',value)
+
+
+    def array_to_double(self,v):
+        num_digits = len(v)
+        # print(v)
+        for i in range(0,num_digits):
+            if(v[i]==46):
+                break
+        upper = v[0:i:1]
+        lower = v[i+1:num_digits:1]
+        # print(upper)
+        # print(lower)
+
+        output = 0
+        power = 0
+        for d in reversed(upper):
+            d = d-48
+            output += (d*(10**power))
+            power += 1
+
+        power = -1
+        for d in lower:
+            d = d-48
+            output += (d*(10**power))
+            power -= 1
+
+        # print(output)
+        return output
+
+
+    def is_digit(self,c):
+        return ((c >= 48) and (c <= 57))
+
+    def advance(self):
+        self.current = self.current+1
+        return (self.byte_arr)[self.current-1]
+
+    def match(self,expected):
+        if(self.is_at_end()):
+            return False
+        if((self.byte_arr)[self.current] != expected):
+            return False
+
+        self.current += 1
+        return True
+
+    def peek(self):
+        if(self.is_at_end()):
+            return '\0'
+        return (self.byte_arr)[self.current]
+
+    def peek_next(self):
+        if(self.current+1 >= len(self.byte_arr)):
+            return 0
+        return self.byte_arr[self.current+1]
+
+    def scan_string(self):
+        while(self.peek() != 34 and (self.is_at_end() == False)):
+            if(self.peek() == 10):
+                self.line += 1
+            self.advance()
+
+        if(self.is_at_end()):
+            Z.error_message(self.line,"unterminated string")
+            return
+
+        self.advance()
+
+        value = self.byte_arr[self.start+1:self.current-1:1]
+        value_bytes = bytes(value)
+        print(value_bytes)
+        # for i in range(0,len(value)):
+        #     integer_val = value[i]
+        #     byte_val = byte(integer_val)
+        #     value_bytes[i] = byte_val
+
+        # b''.join(value_bytes).decode('utf-8')
+
+        self.add_token('string',value_bytes)
+    # def add_token(type):
+    #     add_token(type,None)
+
+    def add_token(self,type,literal):
+        # print("adding token")
+        text = (self.byte_arr)[self.start:self.current:1]
+        (self.tokens).append(Token(type,text,literal,self.line))
+
+
+
+
+class Token:
+    def __init__(self,type,lexeme,literal,line):
+        self.type = type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
--- zeta.py
+++ zeta.py
@@ -2,13 +2,13 @@ import sys
 import os
 from array import array
 from enum import Enum
-
+from abc import ABC, abstractmethod
+import scanner#scanner.py
 
 
 class Zeta:
     def __init__(self):
         self.has_error = False
-        #self.token_type = Enum("LPAREN","RPAREN","LBRACE","RBRACE","COMMA","DOT","MINUS","PLUS","SEMICOLON","SLASH","STAR","EXLAMATION","EXLAMATION_EQUAL","EQUAL","EQUAL_EQUAL","GREATER","GREATER_EQUAL","LESS","LESS_EQUAL","IDENTIFIER","STRING","NUMBER","AND","CLASS","ELSE","FALSE","FUN","FOR","IF","NIL","OR","PRINT","RETURN","SUPER","THIS","TRUE","VAR","WHILE")
 
     def error_message(self,line,message):
         print("ERROR: line",line,":",message)
@@ -18,12 +18,9 @@ class Zeta:
 
 Z = Zeta()
 
+
+
 def main():
-
-
-
-
-    Z.error_message(4,"test error message")
 
     if(len(sys.argv) > 2):
         print("Usage: zeta [script]")
@@ -44,23 +41,26 @@ def main():
 def run_file(filename):
     file_size_bytes = os.path.getsize(filename)
     f = open(filename,"rb")
-    b = array('b')
+    # b = array('b')
+    b = []
 
     for i in range(0,file_size_bytes):
         #print(f.read(1))
         c = f.read(1)
         b.append(int.from_bytes(c,"big"))
 
+
+
     f.close
 
     s = Scanner(b)
     token_list = s.scan_tokens()
 
-    # for t in token_list:
-    #     print(t)
-    #uncommment later
+    # for t in self.token_list:
+    #     print(t.type)
 
-    s.print_byte_arr()
+    print_all_tokens(token_list)
+
 
 
     run(b)
@@ -70,6 +70,14 @@ def run_file(filename):
 
     return
 
+def print_all_tokens(token_list):
+    for t in token_list:
+        print(t.type,end=' ')
+        if(t.type == 'number' or t.type=='string' or t.type=='identifier'):
+            print(t.literal,end='')
+        print('')
+
+
 
 
 def run(byte_arr):
@@ -77,147 +85,36 @@ def run(byte_arr):
     return
 
 
+class Expression(ABC):
+    def __init__(self):
+        pass
 
+class Binary(Expression, left, operator, right):
+    def __init__(self):
+        self.left = left
+        self.operator = operator
+        self.right = right
 
-class Scanner:
-    def __init__(self,byte_arr):
-        self.start = 0
-        self.current = 0
-        self.line = 1
+class Unary(Expression,operator,right):
+    def __init__(self):
+        self.operator = operator
+        self.right = tight
 
-        self.byte_arr = byte_arr#called 'source' in book
-        self.tokens = []
+class Grouping(Expression,expression):
+    def __init__(self):
+        self.expression = expression
 
+class Literal(Expression,value):
+    def __init__(self):
+        self.value = value
 
-    def print_byte_arr(self):
-        for i in range(0,len(self.byte_arr)):
-            print(self.byte_arr[i])
+class Operator(Expression,value):
+    def __init__(self):
+        self.value = value
 
-    def is_at_end(self):
-        return (self.current >= len(self.byte_arr))
-
-    def scan_tokens(self):
-        print("scanning tokens")
-        while(self.is_at_end() == False):
-            self.start = self.current
-            self.scan_token()
-
-
-        (self.tokens).append(Token("EOF","",None,None))#I think?
-        #in book it is (EOF,"","NULL","line")
-        return(self.tokens)
-        #must return some list of token objects
-
-    def scan_token(self):
-        c = self.advance()
-        if(c==40):
-            self.add_token('(',None)
-        elif(c==41):
-            self.add_token('(',None)
-        elif(c==123):
-            self.add_token('{',None)
-        elif(c==125):
-            self.add_token('}',None)
-        elif(c==44):
-            self.add_token(',',None)
-        elif(c==46):
-            self.add_token('.',None)
-        elif(c==45):
-            self.add_token('-',None)
-        elif(c==43):
-            self.add_token('+',None)
-        elif(c==59):
-            self.add_token(';',None)
-        elif(c==42):
-            self.add_token('*',None)
-        elif(c==33):
-            if(self.match(61)):
-                self.add_token('!=',None)
-            else:
-                self.add_token('!',None)
-        elif(c==61):
-            if(self.match(61)):
-                self.add_token('==',None)
-            else:
-                self.add_token('=',None)
-        elif(c==60):
-            if(self.match(61)):
-                self.add_token('<=',None)
-            else:
-                self.add_token('<',None)
-        elif(c==62):
-            if(self.match(61)):
-                self.add_token('>=',None)
-            else:
-                self.add_token('>',None)
-        elif(c==47):
-            if(self.match(47)):
-                while(self.peek() != '\n' and (self.is_at_end() == False)):
-                    self.advance()
-            else:
-                self.add_token('/',None)
-        elif(c==32):#space
-            pass
-        elif(c==9):#tab
-            pass
-        elif(c==13):#carriage return
-            pass
-        elif(c==10):#newline
-            self.line += 1
-        elif(c==34):#double quotes
-            self.scan_string()
-        else:
-            Z.error_message(self.line,"unexpected character")
-
-    def advance(self):
-        self.current = self.current+1
-        return (self.byte_arr)[self.current-1]
-
-    def match(self,expected):
-        if(self.is_at_end()):
-            return False
-        if((self.byte_arr)[self.current] != expected):
-            return False
-
-        self.current += 1
-        return True
-
-    def peek(self):
-        if(self.is_at_end()):
-            return '\0'
-        return (self.byte_arr)[self.current]
-
-    def scan_string(self):
-        while(self.peek() != 34 and (self.is_at_end() == False)):
-            if(self.peek() == 10):
-                self.line += 1
-            self.advance()
-
-        if(self.is_at_end()):
-            Z.error_message(self.line,"unterminated string")
-            return
-
-        self.advance()
-
-        value = self.byte_arr[self.start+1:self.current-1:1]
-        self.add_token('string',value)
-    # def add_token(type):
-    #     add_token(type,None)
-
-    def add_token(self,type,literal):
-        print("adding token")
-        text = (self.byte_arr)[self.start:self.current:1]
-        (self.tokens).append(Token(type,text,literal,self.line))
-
-
-
-
-class Token:
-    def __init__(self,type,lexeme,literal,line):
-        self.type = type
-        self.lexeme = lexeme
-        self.literal = literal
-        self.line = line
+class tree_printer:
+    def string_print(Expression expr):
+        return expr.accept(self)
 
 
 
